7fde0ab0397ce3056e720d3d420389561b721c33
vsakun <vsakun@gmail.com>
2018-02-04 16:13:53 +0300

    update

--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@ $(document).ready(function(){
 при прокручивании до него, а так же описание поведения верхнего меню*/
 var objToStick = $("nav"); //Получаем нужный объект
 var topOfObjToStick = $(objToStick).offset().top; //Получаем начальное расположение нашего блока
-var allObj = $(".glob_mn,.smal_mn,.back_mn");
 
 $(window).scroll(function () {
 	var windowScroll = $(window).scrollTop(); //Получаем величину, показывающую на сколько прокручено окно
@@ -23,12 +22,14 @@ $(window).scroll(function () {
   	$(".logo-nav").addClass("logo-nav-width");
   	$(".logo").addClass("logo-width");
   	$(".click-logo").fadeIn();
+  	$(".phone-mail").fadeOut();
   } 
   else {
   	$("nav").parent().parent().fadeIn();
   	$(".logo-nav").removeClass("logo-nav-width");
   	$(".logo").removeClass("logo-width");
   	$(".click-logo").fadeOut();
+  	$(".phone-mail").fadeIn();
   };
 });
 /* Выезжающее меню */
@@ -38,50 +39,10 @@ function mnuslow(){
 		$(".logo-nav").removeClass("logo-nav-width");
 		$(".logo").removeClass("logo-width");
 		$(".click-logo").fadeOut();
+		$(".phone-mail").fadeIn();
 	});
 };
 mnuslow();
-/* перемещение по секциям */
-/*function move(){
-	var an = $('a[href*="#"]');
-	var a0 = an[0];
-	var a1 = an[1];
-	var a2 = an[2];
-	var a3 = an[3];
-	var a4 = an[4];
-	var a5 = an[5];
-	var t0 = ($(a0).attr('href'));
-	var t1 = ($(a1).attr('href'));
-	var t2 = ($(a2).attr('href'));
-	var t3 = ($(a3).attr('href'));
-	var t4 = ($(a4).attr('href'));
-	var t5 = ($(a5).attr('href'));
-	$(window).scroll(function(){
-		var s_top = $(window).scrollTop();
-		if($(t0).offset().top == s_top){
-		
-		}
-		else if($(t1).offset().top == s_top){
-
-		}
-		else if($(t2).offset().top == s_top){
-			alert($(t2).offset().top);
-		}
-		else if($(t3).offset().top == s_top){
-			alert($(t3).offset().top);
-		}
-		else if($(t4).offset().top == s_top){
-			alert($(t4).offset().top);
-		}
-		else if($(t5).offset().top == s_top){
-			alert($(t5).offset().top);
-		}
-		else{
-
-		};
-	});
-};
-move();*/
  
 /* ОТслеживание ширины окна браузера */
 function windowSize(){
